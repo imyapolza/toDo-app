ed141987e730190d978708fcb0c86f33c52125ad
VinerGat <94351574+imyapolza@users.noreply.github.com>
2022-02-15 03:53:49 +0500

    Update Sort.tsx

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTypedSelector } from '../redux/hooks/useTypedSelector';
 
 interface SortProps {
   onClickSortType: (input: string) => void;
@@ -17,10 +18,15 @@ export const Sort: React.FC<SortProps> = ({ onClickSortType }: SortProps): React
     setVisiblePopup(false);
   };
 
+  const type = useTypedSelector(({ filter }) => filter);
+
   return (
     <>
       <b className="sort" onClick={() => setVisiblePopup(!visiblePopup)}>
         Сортировать по:
+        <div>
+          {type === 'completed' ? 'выполненым' : type === 'noCompleted' ? 'невыполненым' : 'Всем'}
+        </div>
       </b>
       {visiblePopup &&
         items.map((obj, index: number) => (
